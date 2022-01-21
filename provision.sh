882b5541939a31a606e4e265d3a0ebd5aa527098
jack1142 <6032823+jack1142@users.noreply.github.com>
2022-01-21 10:01:14 +0100

    Use gnome-session-inhibit instead of systemd-inhibit

--- provision.sh
+++ provision.sh
@@ -9,7 +9,7 @@ git pull --ff-only || exit 1
 ansible-galaxy install -r ./requirements.yaml
 
 # Run playbook.
-systemd-inhibit ansible-playbook --ask-become-pass ./desktop.yaml
+gnome-session-inhibit ansible-playbook --ask-become-pass ./desktop.yaml
 
 # Notify after finishing.
 notify-send --icon=terminal --urgency=critical 'Machine provisioned!' 'Ansible finished running.'
