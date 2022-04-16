7758419eda98b064d60fb5ffd2d25cc400956370
jack1142 <6032823+jack1142@users.noreply.github.com>
2022-04-16 22:20:02 +0200

    Make sure libnotify-bin is present before running provision script

--- install_and_provision.sh
+++ install_and_provision.sh
@@ -4,5 +4,8 @@ sudo add-apt-repository -y ppa:ansible/ansible
 sudo apt update
 sudo apt install -y ansible
 
+# Install libnotify-bin.
+sudo apt install -y libnotify-bin
+
 # Execute Ansible playbook through provision.sh script.
 exec bash "$(dirname "$(readlink -f "$0")")/provision.sh"
